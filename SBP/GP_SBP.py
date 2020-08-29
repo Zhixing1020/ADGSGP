1163c28ffe72fc513afe8a1584ae0f87743e31e3
ZhixingH <41175145+Zhixing1020@users.noreply.github.com>
2020-08-29 17:18:22 +0800

    Debug in SR experiment
    add some formating modules and the file statistic module (GP_SBP.py & StatisticFile.py)

--- SBP/GP_SBP.py
+++ SBP/GP_SBP.py
@@ -16,7 +16,7 @@
 import operator
 import math
 import random
-
+import time
 import numpy
 
 from deap import algorithms
@@ -25,16 +25,21 @@ from deap import creator
 from deap import tools
 from deap import gp
 from deap import semantic as sm
+from deap import StatisticFile
 
 import decimal
 decimal.getcontext().prec = 100
 
 # Define new functions
 def protectedDiv(left, right):
-    try:
-        return left / right
-    except ZeroDivisionError:
-        return 1
+    if right == 0:
+        return left
+    res = left / right
+    if res > 1e7:
+        return 1e7
+    if res < -1e7:
+        return -1e7
+    return res
 
 def protectedMul(left, right):
     try:
@@ -42,97 +47,79 @@ def protectedMul(left, right):
     except OverflowError:
         return 1e7
 
-pset = gp.PrimitiveSet("MAIN", 2)
+def protectedLog(arg):
+    if abs(arg) < 1e-5:
+        arg = 1e-5
+    return math.log(abs(arg))
+
+def protectedExp(arg):
+    if arg > 10:
+        arg = 10
+    return math.exp(arg)
+
+
+pset = gp.PrimitiveSet("MAIN", 1)
 pset.addPrimitive(operator.add, 2)
 pset.addPrimitive(operator.sub, 2)
 #pset.addPrimitive(operator.mul, 2)
 pset.addPrimitive(protectedMul, 2, name="mul")
 pset.addPrimitive(protectedDiv, 2, name="div")
 #pset.addPrimitive(operator.neg, 1)
-#pset.addPrimitive(math.cos, 1)
-#pset.addPrimitive(math.sin, 1)
-pset.addEphemeralConstant("rand101", lambda: random.randint(-1,1))
-pset.renameArguments(ARG0='x')
-pset.renameArguments(ARG1='y')
+pset.addPrimitive(math.cos, 1, name="cos")
+pset.addPrimitive(math.sin, 1, name="sin")
+pset.addPrimitive(protectedExp, 1, name= "exp")
+pset.addPrimitive(protectedLog, 1, name="ln")
+#pset.addEphemeralConstant("rand101", lambda: random.randint(-1,1))
+#pset.renameArguments(ARG0='x')
+#pset.renameArguments(ARG1='y')
 
 
 creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
 creator.create("Individual", gp.PrimitiveTree, fitness=creator.FitnessMin, sem_vec=numpy.zeros(180))
 
 toolbox = base.Toolbox()
-toolbox.register("expr", gp.genHalfAndHalf, pset=pset, min_=3, max_=5)
+toolbox.register("expr", gp.genHalfAndHalf, pset=pset, min_=2, max_=6)
 toolbox.register("individual", tools.initIterate, creator.Individual, toolbox.expr)
 toolbox.register("population", tools.initRepeat, list, toolbox.individual)
 toolbox.register("compile", gp.compile, pset=pset)
 
-def tarFun(points):
-    tar_sem = numpy.array(list(x[0]*x[1]+math.sin((x[0]-1)*(x[1]-1)) for x in points))
-    return tar_sem
-
-def evalSymbReg(individual, points):
+def evalSymbReg(individual, points, outputs):
     # Transform the tree expression in a callable function
     func = toolbox.compile(expr=individual)
     # Evaluate the mean squared error between the expression
     # and the real function : x**4 + x**3 + x**2 + x
     #sqerrors = ((func(x) - x**4 - x**3 - x**2 - x)**2 for x in points)
-    semanVe = numpy.array(list(func(x[0], x[1]) for x in points))
-    sqerrors = tuple((decimal.Decimal(func(x[0], x[1])) - decimal.Decimal(x[0]*x[1]+math.sin((x[0]-1)*(x[1]-1)))) ** 2 for x in points)
-    res = (math.fsum(sqerrors) / len(points), semanVe)
-    return (math.fsum(sqerrors) / len(points),semanVe),
+    #semanVe = numpy.array(list(func(x[0], x[1]) for x in points))
+    semanVe = numpy.array(list(func(x[0]) for x in points))
+    sqerrors = []
+    for x, y in zip(points, outputs):
+        #sqerrors.append((decimal.Decimal(func(x[0], x[1])) - decimal.Decimal(y)) ** 2)
+        sqerrors.append((decimal.Decimal(func(x[0])) - decimal.Decimal(y)) ** 2)
 
-#toolbox.register("evaluate", evalSymbReg, points=[x/10. for x in range(-10,10)])
-indata=[]
-for i in range(0,180):
-    x=random.randrange(-10,10)/10.
-    y=random.randrange(-10,10)/10.
-    indata.append((x,y))
-toolbox.register("evaluate", evalSymbReg, points=indata)
-toolbox.register("select", tools.selTournament, tournsize=3)
-#toolbox.register("mate", gp.cxOnePoint)
-#toolbox.register("expr_mut", gp.genFull, min_=0, max_=2)
-#toolbox.register("mutate", gp.mutUniform, expr=toolbox.expr_mut, pset=pset)
-#toolbox.register("SCX", gp.cxSemanticTest, pset=pset)
-#toolbox.register("SMT", gp.mutSemanticTest, pset=pset)
-toolbox.register("SCR", sm.semConRep, toolbox=toolbox, pset=pset, points=indata)
+    return (math.sqrt(math.fsum(sqerrors) / len(points)),semanVe),
+
+toolbox.register("select", tools.selTournament, tournsize=2)
+toolbox.register("selectBest", tools.selBest)
 toolbox.register("ADS", sm.angleDrivenSel, toolbox=toolbox)
 toolbox.register("PC", sm.perpendicularCX)
 toolbox.register("RSM", sm.randSegMut)
 
-#toolbox.decorate("mate", gp.staticLimit(key=operator.attrgetter("height"), max_value=17))
-#toolbox.decorate("mutate", gp.staticLimit(key=operator.attrgetter("height"), max_value=17))
-toolbox.decorate("SCR", gp.staticLimit(key=operator.attrgetter("height"), max_value=17))
-#toolbox.decorate("SCX", gp.staticLimit(key=operator.attrgetter("height"), max_value=17))
-#toolbox.decorate("SMT", gp.staticLimit(key=operator.attrgetter("height"), max_value=17))
+def loadData(flieName):
+  inFile = open(flieName, 'r')
+  X = []
+  y = []
+  datasize = int(inFile.readline().split('\t')[0])
+  for i in range(0,datasize):
+      line=inFile.readline()
+      trainingSet = line.split('\t')
+      x=[]
+      for xi in trainingSet[:-1]:
+          x.append(float(xi))
+      X.append(tuple(x))
+      y.append(float(trainingSet[-1]))
+  return X, y
 
 def main():
-    random.seed(318)
-
-    pop = toolbox.population(n=1000)
-    hof = tools.HallOfFame(1)
-
-    #test
-    #lib = sm.library(toolbox, pset=pset, points=indata)
-    #testa=toolbox.expr()
-    #lib.lib_maintain(pop)
-    #ex=creator.Individual(toolbox.expr())
-    #print(ex)
-    #ex3=sm.semConRep(ex,17,AL=[],tarSem=tarFun(indata),toolbox=toolbox,pset = pset,points = indata, library=lib)
-    #print(ex3)
-    #parent, children = sm.decode(ex)
-    #nst = [pset.mapping['add'], 20, pset.mapping['mul'], 10, ex]
-    #ex2 = creator.Individual(nst)
-    #print(ex2)
-    #print(creator.Individual(ex[ex.searchSubtree(4)]).__str__())
-    #code = str(ex)
-    #print(gp.compile(code, pset))
-    #sub_tree=sm.subTree(toolbox, ex, pset, indata)
-    #lib.insert_lib(sub_tree)
-    #lib.lib_maintain(pop)
-    #tar_sem = tarFun(points=indata)
-    #sel_pairs = sm.angleDrivenSel(toolbox,pop=pop,tarSem=tar_sem, np=5,nt=5,ta=90)
-    #dsr_sem=[]
-    #print(numpy.hstack([dsr_sem,0]))
-    #print(ex[3])
 
     stats_fit = tools.Statistics(lambda ind: ind.fitness.values)
     stats_size = tools.Statistics(len)
@@ -142,13 +129,61 @@ def main():
     mstats.register("min", numpy.min)
     mstats.register("max", numpy.max)
 
-    lib = sm.library(toolbox, pset=pset, points=indata)
-    lib.lib_maintain(pop)
+    avg_fit = 0
+    suc_rate = 0
+    avg_size = 0
 
-    pop, log = algorithms.semanticGP(pop, toolbox, tarSem=tarFun(indata), library=lib, ngen=1000, stats=mstats,
-                                   halloffame=hof, verbose=True)
-    print(hof.items[0])
-    print(hof.items[0].fitness)
+    SF = StatisticFile.StatisticFile()
+
+    for run in range(0,4):
+        SF.problemInit(30)
+        for ei in range(0, 30):
+            random.seed(318 + ei)
+
+            SF.independentRunInit(run, ei)
+            suc = 0
+            eva_times = 0
+            train_err = 1e6
+            test_err = 1e6
+            prog_size = 0
+            traintime = 0
+
+            filename = "D:\\exp_data\\SLGP_for_SR\\F%d_%d_training_data.txt" % (run, ei % 10)
+            indata, outdata = loadData(filename)
+            #remember to update the __init__ function of class subTree
+
+            toolbox.register("evaluate", evalSymbReg, points=indata, outputs=outdata)
+            toolbox.register("SCR", sm.semConRep, toolbox=toolbox, pset=pset, points=indata)
+            toolbox.decorate("SCR", gp.staticLimit(key=operator.attrgetter("height"), max_value=17))
+
+            time_start = time.time()
+
+            pop = toolbox.population(n=100)
+            hof = tools.HallOfFame(1)
+            lib = sm.library(toolbox, pset=pset, points=indata)
+
+            pop, log, eva_times = algorithms.semanticGP(pop, toolbox, tarSem=outdata, library=lib, output_file=SF, ngen=100000,
+                                             stats=mstats, halloffame=hof, verbose=True)
+
+            time_end = time.time()
+
+            if hof.items[0].fitness.values[0] < 1e-4:
+                suc = 1
+            train_err = hof.items[0].fitness.values[0]
+            prog_size = hof.items[0].__len__()
+            traintime = time_end - time_start
+
+            #testing
+            filename = "D:\\exp_data\\SLGP_for_SR\\F%d_%d_testing_data.txt" % (run, ei % 10)
+            indata, outdata = loadData(filename)
+            test_err = evalSymbReg(hof.items[0], indata, outdata)[0][0]
+
+            print(hof.items[0])
+            print(hof.items[0].fitness)
+
+            SF.independentRunRecord(run,ei,suc,eva_times,train_err,test_err,prog_size,hof.items[0].__str__(),traintime)
+        SF.problemOverallRecord(run)
+
 
     return pop, log, hof
 
---
+++ deap/StatisticFile.py
@@ -0,0 +1,131 @@
+from __future__ import division
+import numpy as np
+
+class StatisticFile(object):
+    suc = []
+    train_error = []
+    test_error = []
+    eva_times = []
+    prog_size = []
+    trainTime = []
+
+    conv_internal = 50000
+    last_internal = 0
+
+    overallAdd = "..\\overall.txt"
+    fitness_convAdd = "..\\fitness_conv.txt"
+    fitness_timesAdd = "..\\fitness_time.txt"
+    train_errorAdd = "..\\train_error.txt"
+    test_errorAdd = "..\\test_error.txt"
+    prog_sizeAdd = "..\\prog_size.txt"
+    example_progAdd = "..\\example_prog.txt"
+    trainTimeAdd = "..\\trainTime.txt"
+
+    def __init__(self):
+        self.initFile()
+
+    def initFile(self):
+        doc = open(self.overallAdd, 'w')
+        doc.close()
+        doc = open(self.fitness_convAdd, 'w')
+        doc.close()
+        doc = open(self.fitness_timesAdd, 'w')
+        doc.close()
+        doc = open(self.train_errorAdd, 'w')
+        doc.close()
+        doc = open(self.test_errorAdd, 'w')
+        doc.close()
+        doc = open(self.prog_sizeAdd, 'w')
+        doc.close()
+        doc = open(self.example_progAdd, 'w')
+        doc.close()
+        doc = open(self.trainTimeAdd, 'w')
+        doc.close()
+
+    def problemInit(self, iterative):
+        self.suc = np.zeros(iterative)
+        self.train_error = np.zeros(iterative)
+        self.test_error = np.zeros(iterative)
+        self.eva_times = np.zeros(iterative)
+        self.prog_size = np.zeros(iterative)
+        self.trainTime = np.zeros(iterative)
+
+    def independentRunInit(self, run, job):
+        self.last_internal = 0
+
+        doc = open(self.fitness_convAdd, 'a')
+        print("\n%d\t%d" % (run, job), file=doc)
+        doc.close()
+        doc = open(self.fitness_timesAdd, 'a')
+        print("%d\t%d\t" % (run, job), file=doc, end='')
+        doc.close()
+        doc = open(self.train_errorAdd, 'a')
+        print("%d\t%d\t" % (run, job), file=doc, end='')
+        doc.close()
+        doc = open(self.test_errorAdd, 'a')
+        print("%d\t%d\t" % (run, job), file=doc, end='')
+        doc.close()
+        doc = open(self.prog_sizeAdd, 'a')
+        print("%d\t%d\t" % (run, job), file=doc, end='')
+        doc.close()
+        doc = open(self.example_progAdd, 'a')
+        print("%d\t%d" % (run, job), file=doc)
+        doc.close()
+        doc = open(self.trainTimeAdd, 'a')
+        print("%d\t%d\t" % (run, job), file=doc, end='')
+        doc.close()
+
+    def procedureRecord(self, eva, fitness):
+        doc = open(self.fitness_convAdd, 'a')
+        recordeval = eva // self.conv_internal
+        if self.last_internal == 0:
+            print("%d\t%f"%(self.last_internal, fitness), file=doc)
+        for i in range(1+self.last_internal, recordeval + 1):
+            print("%d\t%f" % (i*self.conv_internal, fitness), file=doc)
+        self.last_internal = recordeval
+        doc.close()
+
+    def independentRunRecord(self, run, job, sucFlag, f_times, train_err, test_err, prog_siz, example_prog, train_time):
+        self.suc[job] = sucFlag
+        self.eva_times[job] = f_times
+        self.train_error[job] = train_err
+        self.test_error[job] = test_err
+        self.prog_size[job] = prog_siz
+        self.trainTime[job] = train_time
+
+        print("%d\t%d\tsuc:\t%f" % (run, job, self.suc.sum() / (job + 1)), end='')
+        print("\teva_times:\t%f" % (self.eva_times.sum() / (job+1)), end='')
+        print("\ttrain_error:\t%f" % (self.train_error.sum() / (job+1)), end='')
+        print("\ttest_error:\t%f" % (self.test_error.sum() / (job + 1)), end='')
+        print("\tprog_size:\t%f" % (self.prog_size.sum() / (job + 1)), end='')
+        print("\ttrain_time:\t%f" % (self.trainTime.sum() / (job + 1)))
+
+        doc = open(self.fitness_timesAdd, 'a')
+        print("%f" % (f_times), file=doc)
+        doc.close()
+        self.procedureRecord(f_times,train_err)
+        doc = open(self.train_errorAdd, 'a')
+        print("%f" % (train_err), file=doc)
+        doc.close()
+        doc = open(self.test_errorAdd, 'a')
+        print("%f" % (test_err), file=doc)
+        doc.close()
+        doc = open(self.prog_sizeAdd, 'a')
+        print("%f" % prog_siz, file=doc)
+        doc.close()
+        doc = open(self.example_progAdd, 'a')
+        print("%s\n" % example_prog, file=doc)
+        doc.close()
+        doc = open(self.trainTimeAdd, 'a')
+        print("%f" % train_time, file=doc)
+        doc.close()
+
+    def problemOverallRecord(self, run):
+        doc = open(self.overallAdd,'a')
+        print("%d\tsuc:\t%f" % (run, self.suc.sum() / self.suc.size()), end='', file=doc)
+        print("\teva_times:\t%f" % self.eva_times.sum() / self.eva_times.size, end='', file=doc)
+        print("\ttrain_error:\t%f" % self.train_error.sum() / self.train_error.size, end='', file=doc)
+        print("\ttest_error:\t%f" % self.test_error.sum() / self.test_error.size, end='', file=doc)
+        print("\tprog_size:\t%f" % self.prog_size.sum() / self.prog_size.size, end='', file=doc)
+        print("\ttrain_time:\t%f" % self.trainTime.sum() / self.trainTime.size, file=doc)
+        doc.close()
